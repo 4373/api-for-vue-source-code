b10567411af9116eda8d462c76f508e4053de731
me <mh4373@163.com>
2018-06-11 00:45:45 +0800

    static file

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 var fs = require('fs')
 var path = require('path')
 var http = require('http')
+var parse = require('url').parse
+var join = require('path').join
+
+var root = __dirname
 
 http.createServer(function(req,res) {
-  
-})
+  sendStaticFile(req, res)
+}).listen(8888)
 
-var file = path.join(process.cwd(), '/apilist.json')
+function sendStaticFile(req, res) {
+  var url = parse(req.url)
+  var path = join(root, url.pathname)
+  fs.stat(path, function(err, stat) {
+    if(err) {
+      if('ENOENT' === err.code) {
+        res.statusCode = 404
+        res.end('Not Found')
+      } else {
+        res.statusCode = 500
+        res.end('Internal Server Error')
+      }
+    } else {
+      res.setHeader('Content-Length', stat.size)
+      var stream = fs.createReadStream(path)
+      stream.pipe(res)
+      stream.on('error', function(err) {
+        res.statusCode = 500
+        res.end('Internal Server Error')
+      })
+    }
+  })
+}
+
+
 
 function loadOrInitList(file, cb) {
   fs.exists(file, function(exists) {
